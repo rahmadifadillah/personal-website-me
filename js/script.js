ea171d4f11e2533ebfde29bdfad5eed9080eca78
Rahmadi Fadillah <rahmadifadillah83@gmail.com>
2023-04-04 23:42:37 +0700

    add js responsive

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Active Menu Button 
+let menuBtn = document.querySelector('#menu-btn');
+let menuBar = document.querySelector('.menu-bar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('bx-x');
+    menuBar.classList.toggle('active');
+}
